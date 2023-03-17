5cdea868f78936c5ef8701433cd68e88a9f63132
MinHyeong-Lee <usupool94@gmail.com>
2023-03-17 09:59:05 +0900

    gitignore 수정

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { NestFactory } from '@nestjs/core';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
@@ -14,13 +14,14 @@ async function bootstrap() {
   );
 
   const config = new DocumentBuilder()
-  .setTitle('Swagger-tutorial')
-  .setDescription('user API description')
-  .setVersion('1.0')
-  .build();
+    .setTitle('Swagger-tutorial')
+    .setDescription('user API description')
+    .setVersion('1.0')
+    .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('swagger', app, document)
+  SwaggerModule.setup('swagger', app, document);
   await app.listen(3000);
 }
+console.log(`current process.env.NODE_ENV === ${process.env.NODE_ENV}`);
 bootstrap();
